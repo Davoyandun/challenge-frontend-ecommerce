1a182ea4e0b13a66418b048f77918d169824a250
David Yandún <70674906+Davoyandun@users.noreply.github.com>
2024-01-27 20:29:11 -0500

    fix:use the rating value that comes from the api (#1)
    :star:

--- src/components/Filter/RatingFilter/Rating/index.jsx
+++ src/components/Filter/RatingFilter/Rating/index.jsx
@@ -1,27 +1,32 @@
-import { AiFillStar } from 'react-icons/ai'
-import { AiOutlineStar } from 'react-icons/ai'
-import './Rating.css'
+import { AiFillStar } from "react-icons/ai";
+import { AiOutlineStar } from "react-icons/ai";
+import "./Rating.css";
+import PropTypes from "prop-types";
 
-function Rating ( {stars} ) {
-    const fillStars = () => {
-        const etiquetas = [];
-        const starSelected = [...Array(stars)].map((_, index) => (
-            <AiFillStar className='StarFilled' key={index}/>
-        ))
-        const starNotSelected = [...Array(5-stars)].map((_, index) => (
-            <AiOutlineStar className='StarNotFilled' key={index}/>
-        ))
-        etiquetas.push(starSelected)
-        etiquetas.push(starNotSelected)
-        return etiquetas
-    }
+function Rating({ stars }) {
+  const fillStars = () => {
+    const etiquetas = [];
+    const starSelected = [...Array(stars)].map((_, index) => (
+      <AiFillStar className="StarFilled" key={index} />
+    ));
+    const starNotSelected = [...Array(5 - stars)].map((_, index) => (
+      <AiOutlineStar className="StarNotFilled" key={index} />
+    ));
+    etiquetas.push(starSelected);
+    etiquetas.push(starNotSelected);
+    return etiquetas;
+  };
 
-    return (
-        <div className='RatingContainer'>
-            {fillStars()}
-            <h3>& up</h3>
-        </div>
-    )
+  return (
+    <div className="RatingContainer">
+      {fillStars()}
+      <h3>& up</h3>
+    </div>
+  );
 }
 
-export { Rating }
+Rating.propTypes = {
+  stars: PropTypes.number.isRequired,
+};
+
+export { Rating };
--- src/components/ResultTable/Card/index.jsx
+++ src/components/ResultTable/Card/index.jsx
@@ -1,36 +1,34 @@
-import { useContext } from 'react';
-import { Detail } from './Detail'
-import { SearchContext } from '../../../contexts/SearchContext';
-import './Card.css'
+import { useContext } from "react";
+import { Detail } from "./Detail";
+import { SearchContext } from "../../../contexts/SearchContext";
+import "./Card.css";
 
-function Card ({ image, title, price, description }) {
-    const {
-        setIsOpen,
-        setImageProduct,
-        setTitleProduct,
-        setPriceProduct,
-        setDescriptionProduct,
-    } = useContext(SearchContext);
+function Card({ image, title, price, description, rating }) {
+  const {
+    setIsOpen,
+    setImageProduct,
+    setTitleProduct,
+    setPriceProduct,
+    setDescriptionProduct,
+    setRatingProduct,
+  } = useContext(SearchContext);
 
-    const openModal = () => {
-        setIsOpen(true)
-        setImageProduct(image)
-        setTitleProduct(title)
-        setPriceProduct(price)
-        setDescriptionProduct(description)
-    }
-
-    return (
-        <div className='CardContainer' onClick={openModal}>
-            <div className='ProductImageContainer'>
-                <img src={image}/>
-            </div>
-            <Detail 
-                title = {title}
-                price = {price}
-            />
-        </div>
-    )
+  const openModal = () => {
+    setIsOpen(true);
+    setImageProduct(image);
+    setTitleProduct(title);
+    setPriceProduct(price);
+    setDescriptionProduct(description);
+    setRatingProduct(rating);
+  };
+  return (
+    <div className="CardContainer" onClick={openModal}>
+      <div className="ProductImageContainer">
+        <img src={image} />
+      </div>
+      <Detail title={title} price={price} rating={rating} />
+    </div>
+  );
 }
 
-export { Card }
+export { Card };
